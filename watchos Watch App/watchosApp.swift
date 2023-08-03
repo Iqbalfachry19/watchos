a2732b3bc0a0d98bf66891fbf47072e74e2ce66f
iqbal <ifachry@rocketmail.com>
2023-08-03 15:37:29 +0700

    Initial Commit

---
+++ watchos Watch App/watchosApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchosApp.swift
+//  watchos Watch App
+//
+//  Created by Iqbal Fachry on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct watchos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
